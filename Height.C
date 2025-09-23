6d3b65bb859bde7f874e29888efd6c9a91b9cc3e
stephengithinji738-beep <stephengithinji738@gmail.com>
2025-09-23 12:45:45 +0300

    Add files via upload

---
+++ Height.C
@@ -0,0 +1,16 @@
+/*
+Name: stephen githinji
+Reg no:CT101/G/26434/25
+Description:Enter your height
+*/
+#include <stdio.h>
+
+int main() {
+    float height;
+    
+    printf("enter your height in centimeters:");
+    scanf("%f",&height);
+    printf("your height is: %.2f centimetres\n", height);
+
+    return 0;
+}
