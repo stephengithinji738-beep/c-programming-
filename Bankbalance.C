1afa6c96bdb1237e8e225b9328302d7d5711b6f0
stephengithinji738-beep <stephengithinji738@gmail.com>
2025-09-23 12:52:20 +0300

    Add files via upload

---
+++ Bankbalance.C
@@ -0,0 +1,16 @@
+/*
+Name: stephen githinji
+Reg no:CT101/G/26434/25
+Description:Enter your height
+*/
+#include <stdio.h>
+
+int main() {
+    float Bankbalance;
+    
+    printf("enter your bank balance ia Ksh:");
+    scanf("%f",&Bankbalance);
+    printf("your bank balance is: %.2f Ksh\n", Bankbalance);
+
+    return 0;
+}
