d32d43f6862fd3201420b2f7f9c5c5cb7f213514
stephengithinji738-beep <stephengithinji738@gmail.com>
2025-09-23 13:02:47 +0300

    Add files via upload

---
+++ Phonenumber.C
@@ -0,0 +1,16 @@
+/*
+Name: stephen githinji
+Reg no:CT101/G/26434/25
+Description:Enter your height
+*/
+#include <stdio.h>
+
+int main() {
+    char phone [20];
+    
+    printf("enter your phone number: ");
+    scanf("%19s",phone);
+    printf("your phone number is: %s\n", phone);
+
+    return 0;
+}
