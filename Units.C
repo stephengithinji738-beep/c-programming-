2d454d232913024dbe0b00af7df925da64e0e209
stephengithinji738-beep <stephengithinji738@gmail.com>
2025-09-25 12:08:23 +0300

    Add files via upload

---
+++ Units.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    printf("physics\n");
+    printf("discrete matematics\n");
+    printf("fundermentals of ICT\n");
+    printf("compture artitechture\n");
+    printf("analogue electronics\n");
+    printf("communication skills\n");
+    printf("introduction to programming\n");
+    printf("mathematics for science");
+    
+    
+    return 0;
+}
