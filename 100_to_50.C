a01774a55dadefb1c54512f2c1afd49ea4f0c9d7
stephengithinji738-beep <stephengithinji738@gmail.com>
2025-10-12 12:18:55 +0300

    Add files via upload

---
+++ 100_to_50.C
@@ -0,0 +1,17 @@
+/*
+Name:Stephen Githinji
+REG no:CT101/G/26434/25
+Description:numbers from 100 to 50
+*/
+#include <stdio.h>
+
+int main() {
+    int i;
+    
+    printf("Numbers from 100 down to 50:\n");
+    for (i=100; i>=50;i--){
+        printf("%d\n",i);
+    }
+
+    return 0;
+}
